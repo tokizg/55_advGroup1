2e1cc453636be248ab3b3508403ce4a3f7db9506
tokizg <145384085+tokizg@users.noreply.github.com>
2025-07-11 16:31:34 +0900

    Update loadDrawer.rkt

--- loadDrawer.rkt
+++ loadDrawer.rkt
@@ -1,27 +1,46 @@
 #lang br/quicklang
 
+(require br/datum)
+
+;; リーダ
+;; 各行を (handle "関数名" 引数1 引数2 ...) に変換する
 (define (read-syntax path port)
-  (define src-lines (port->lines port))
-  (define src-datums (format-datums ''(handle ~a) src-lines))
-  (define module-datum `(module loadDraw-mod br "LoadDrawer.rkt"
-                          ,@src-datums))
-  (datum->syntax #f module-datum))
+  (define lines (port->lines port))
+  (define datums
+    (map (lambda (l)
+           (define parts (string-split l))
+           (format-datum
+             `(handle ,@parts)
+             #f))
+         lines))
+  (define mod `(module loadraw-mod "loadraw.rkt" ,@datums))
+  (datum->syntax #f mod))
 (provide read-syntax)
 
-(define-macro (loadDraw-module-begin HANDLE-EXPR ...)
-  #'(#%module-begin
-     HANDLE-EXPR ...))
-(provide (rename-out [loadDraw-module-begin #%module-begin]))
+;; エクスパンダ
+(define-macro (loadraw-module-begin body ...)
+  #'(#%module-begin body ...))
+(provide (rename-out [loadraw-module-begin #%module-begin]))
 
-(define (handle [arg #f])
-    (cond
-        [(equal? arg "直進") (GoSt)]
-        [(equal? arg "T字路") (TJunc)]))
-(provide handle)
+;; 関数名と引数を読んで、適当にRacketの式へ変換する
+(define (handle fn:id . args)
+  (displayln (cons fn args)) ; とりあえず読んだモノを表示しておく
+  (case fn
+    [(関数A) (when (null? args) (FuncA))]
+    [(関数B) (when (= (length args) 1) (FuncB (first args)))]
+    [(関数C) (when (= (length args) 2) (FuncC (first args) (second args)))]
+    [else (error 'handle "Unknown function or wrong args" fn args)]))
 
-(define (GoSt)
-  (display "直進"))
-  (provide GoSt)
+(provide handle FuncA FuncB FuncC)
 
-(define (TJunc direction)
-  (display "T字路を曲がります"))
+;; 仮実装
+;; 関数名と引数を表示する
+(define (FuncA)
+  (displayln 'FuncA)
+  (void))
+(define (FuncB x)
+  (displayln (list 'FuncB x))
+  (void))
+(define (FuncC x y)
+  (displayln (list 'FuncC x y))
+  (void))
